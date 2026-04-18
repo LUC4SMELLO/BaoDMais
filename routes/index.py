a2eaff26b99ac0983ce4e2f06eba33c78d4b3a9d
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-18 20:11:10 -0300

    feat: rota principal

---
+++ routes/index.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+
+index_bp = Blueprint("/", __name__)
+
+@index_bp.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
