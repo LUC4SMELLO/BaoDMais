a229ed9396534d4baf70abaa58ba799826e4c2b0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-18 21:19:32 -0300

    feat: rota estabelecimento

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.pagina_inicial import pagina_inicial_bp
 
 from routes.carrinho import carrinho_bp
 from routes.usuario import usuario_bp
+from routes.estabelecimento import estabelecimento_bp
 
 
 app = Flask(__name__)
@@ -16,6 +17,7 @@ app.register_blueprint(pagina_inicial_bp)
 
 app.register_blueprint(carrinho_bp)
 app.register_blueprint(usuario_bp)
+app.register_blueprint(estabelecimento_bp)
 
 
 if __name__ == "__main__":
