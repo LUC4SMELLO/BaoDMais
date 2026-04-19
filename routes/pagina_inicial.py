8c34bb3e6abb257186a5f82e41d5bc9091fc1070
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-19 11:22:51 -0300

    feat: enviando estabelecimentos para o html

--- routes/pagina_inicial.py
+++ routes/pagina_inicial.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
 
+from models.estabelecimento_model import EstabelecimentoModel
+
 
 pagina_inicial_bp = Blueprint("pagina_inicial", __name__)
 
 @pagina_inicial_bp.route("/pagina_inicial", methods=["GET", "POST"])
 def pagina_inicial():
-    return render_template("pagina_inicial.html")
+
+    estabelecimentos = EstabelecimentoModel.listar_estabelecimentos()
+
+    return render_template("pagina_inicial.html", estabelecimentos=estabelecimentos)
