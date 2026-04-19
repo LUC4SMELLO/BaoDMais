17c1a419aa13078e70f368f8928c8d64224dca99
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-19 16:01:38 -0300

    feat: exibindo card do estabelecimento

--- routes/estabelecimento.py
+++ routes/estabelecimento.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, render_template
 
+from models.estabelecimento_model import EstabelecimentoModel
+
 
 estabelecimento_bp = Blueprint("estabelecimento", __name__)
 
 @estabelecimento_bp.route("/estabelecimento/<int:id>")
 def estabelecimento(id):
-    return render_template("estabelecimento.html", id=id)
+
+    resultado = EstabelecimentoModel.buscar_estabelecimento(id)
+
+    return render_template(
+        "estabelecimento.html",
+        nome=resultado[0],
+        tipo=resultado[1],
+        avaliacao=resultado[2],
+        pedido_minimo=resultado[3],
+        tempo_delivery=resultado[4],
+        caminho_foto=resultado[5]
+        )
