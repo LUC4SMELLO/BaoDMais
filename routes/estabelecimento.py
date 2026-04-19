e741a8527495e745004ca13a7aea39370e0f6520
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-19 14:01:28 -0300

    feat: enviando o id para a rota estabelecimento

--- routes/estabelecimento.py
+++ routes/estabelecimento.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template
 
 estabelecimento_bp = Blueprint("estabelecimento", __name__)
 
-@estabelecimento_bp.route("/estabelecimento")
-def estabelecimento():
-    return render_template("estabelecimento.html")
+@estabelecimento_bp.route("/estabelecimento/<int:id>")
+def estabelecimento(id):
+    return render_template("estabelecimento.html", id=id)
