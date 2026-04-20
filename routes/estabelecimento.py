44c4fba4d2c62c2357a510f08d501cab759ee007
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-19 21:38:06 -0300

    feat: exibindo o cardápio do estabelecimento

--- routes/estabelecimento.py
+++ routes/estabelecimento.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 
 from models.estabelecimento_model import EstabelecimentoModel
 
+from scripts.montar_cardapio import montar_cardapio
+
 
 estabelecimento_bp = Blueprint("estabelecimento", __name__)
 
@@ -9,6 +11,7 @@ estabelecimento_bp = Blueprint("estabelecimento", __name__)
 def estabelecimento(id):
 
     resultado = EstabelecimentoModel.buscar_estabelecimento(id)
+    cardapio = montar_cardapio(id)
 
     return render_template(
         "estabelecimento.html",
@@ -17,5 +20,6 @@ def estabelecimento(id):
         avaliacao=resultado[2],
         pedido_minimo=resultado[3],
         tempo_delivery=resultado[4],
-        caminho_foto=resultado[5]
+        caminho_foto=resultado[5],
+        cardapio=cardapio.values()
         )
