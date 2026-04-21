26ca78a92fd0ad925ad6507b8770d661dc95b3a3
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-21 10:32:18 -0300

    feat: rota item

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from routes.pagina_inicial import pagina_inicial_bp
 from routes.carrinho import carrinho_bp
 from routes.usuario import usuario_bp
 from routes.estabelecimento import estabelecimento_bp
+from routes.item import item_bp
 
 
 app = Flask(__name__)
@@ -18,6 +19,7 @@ app.register_blueprint(pagina_inicial_bp)
 app.register_blueprint(carrinho_bp)
 app.register_blueprint(usuario_bp)
 app.register_blueprint(estabelecimento_bp)
+app.register_blueprint(item_bp)
 
 
 if __name__ == "__main__":
---
+++ routes/item.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+from models.item_model import ItemModel
+
+
+item_bp = Blueprint("item", __name__)
+
+@item_bp.route("/item/<int:id>/<int:id_estabelecimento>", methods=["GET"])
+def item(id, id_estabelecimento):
+    
+    item = ItemModel.buscar_item(id, id_estabelecimento)
+
+    return render_template(
+        "item.html",
+        id=id,
+        id_estabelecimento=id_estabelecimento,
+        nome=item[0],
+        descricao=item[1]
+        )
