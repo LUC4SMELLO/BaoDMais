708a5abcd67774c78b3192de62716c0c1f036583
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-21 12:56:41 -0300

    feat: rota item exibindo as opções

--- routes/item.py
+++ routes/item.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 
 from models.item_model import ItemModel
 
+from scripts.montar_opcoes import montar_opcoes
+
 
 item_bp = Blueprint("item", __name__)
 
@@ -9,11 +11,13 @@ item_bp = Blueprint("item", __name__)
 def item(id, id_estabelecimento):
     
     item = ItemModel.buscar_item(id, id_estabelecimento)
+    opcoes = montar_opcoes(id)
 
     return render_template(
         "item.html",
         id=id,
         id_estabelecimento=id_estabelecimento,
         nome=item[0],
-        descricao=item[1]
+        descricao=item[1],
+        opcoes=opcoes.values()
         )
