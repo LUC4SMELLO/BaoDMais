b1af7f870b6b868ae635cfdc59747aed2b4a3fe0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-25 17:34:51 -0300

    feat: recebendo os dados do usuário

--- routes/login.py
+++ routes/login.py
@@ -1,8 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
+
+from models.estabelecimento_model import EstabelecimentoModel
 
 
 login_bp = Blueprint("login", __name__)
 
 @login_bp.route("/login", methods=["GET", "POST"])
 def login():
+
+    if request.method == "POST":
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+        print(email, senha)
+
+        estabelecimentos = EstabelecimentoModel.listar_estabelecimentos()
+
+        return render_template("pagina_inicial.html", estabelecimentos=estabelecimentos)
+
+
     return render_template("login.html")
