9c47de29244ee128eb28267ceee2777c3e067ed0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-25 17:30:05 -0300

    feat: recebendo os dados do usuário

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -1,8 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, render_template
+
+from models.estabelecimento_model import EstabelecimentoModel
 
 
 cadastro_bp = Blueprint("cadastro", __name__)
 
 @cadastro_bp.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+
+    if request.method == "POST":
+        nome = request.form.get("nome_completo")
+        email = request.form.get("email")
+        senha = request.form.get("senha")
+
+        print(nome, email, senha)
+
+        estabelecimentos = EstabelecimentoModel.listar_estabelecimentos()
+
+        return render_template("pagina_inicial.html", estabelecimentos=estabelecimentos)
+
+
     return render_template("cadastro.html")
