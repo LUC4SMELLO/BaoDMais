1a60efb58765eaa984759c878fa3d529b3aa3639
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-04-25 14:53:07 -0300

    feat: rota login

---
+++ routes/login.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+
+login_bp = Blueprint("login", __name__)
+
+@login_bp.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
