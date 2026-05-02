69449410c1cca22d39bc32a7f1ba595102b7d13e
Lucas Mello <lucaspereirasilvamello@gmail.com>
2026-05-01 21:14:00 -0300

    feat: exibindo o carrinho de compras

--- routes/carrinho.py
+++ routes/carrinho.py
@@ -1,8 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 
 carrinho_bp = Blueprint("carrinho", __name__)
 
 @carrinho_bp.route("/carrinho", methods=["GET", "POST"])
 def carrinho():
-    return render_template("carrinho.html")
+
+    carrinho = session.get("carrinho", [])
+
+    try:
+        nome_estabelecimento = carrinho[0]["nome_estabelecimento"]
+    except Exception:
+        nome_estabelecimento = ""
+    
+    return render_template("carrinho.html", carrinho=carrinho, nome_estabelecimento=nome_estabelecimento)
